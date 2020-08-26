d07b88181d4b0a4cda4b3da6bf5e313d86bc03a8
caiseyann <caiseyann4@gmail.com>
2020-08-26 16:04:25 +0300

    gif component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GifComponent } from './gif/gif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifComponent
   ],
   imports: [
     BrowserModule
