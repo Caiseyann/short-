c875a480063b842c0aef6557e65c9e994246b660
caiseyann <caiseyann4@gmail.com>
2020-08-27 12:25:37 +0300

    added required components on search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+searchIt:string;
+@Output() emitSearch = new EventEmitter<any>()
 
   constructor() { }
 
-  ngOnInit(): void {
+  search(){
+    this.emitSearch.emit(this.searchIt);
+
+  }
+
+  ngOnInit() {
   }
 
 }
